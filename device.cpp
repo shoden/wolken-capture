760adf38223ccf4c70ffe96bf0010181f3200cf3
Juan Roldan Ruiz <juan.roldan@gmail.com>
2011-04-07 09:30:08 +0200

    limpieza

--- device.cpp
+++ device.cpp
@@ -18,8 +18,8 @@ int Device::open()
   fd = v4l2_open(device.toUtf8().data() , O_RDWR, 0);
 
   if(fd < 0) {
-      fprintf(stderr, "No se pudo abrir el dispositivo %s: %s\n", device.toUtf8().data(), strerror(errno));
-      return EXIT_FAILURE;
+    fprintf(stderr, "No se pudo abrir el dispositivo %s: %s\n", device.toUtf8().data(), strerror(errno));
+    return EXIT_FAILURE;
   }
 
   getParams();
@@ -60,12 +60,14 @@ int Device::capture(const QString &file)
   img = cvQueryFrame( cap );
   img = cvQueryFrame( cap );
   img = cvQueryFrame( cap );
+  img = cvQueryFrame( cap );
+
   if( !img ) {
     fprintf(stderr,"No se pudo capturar la imagen\n");
     exit(EXIT_FAILURE);
   }
 
-  // ROI
+  // Región de interés
   if(roi)
     cvSetImageROI(img, roiRect);
 
@@ -78,7 +80,6 @@ int Device::capture(const QString &file)
 
   // Crear miniatura
   QString cmd = QString("convert %1%2 -resize 128x96 %1/thumbs%2").arg(dir.path()).arg(file);
- // cout << cmd.toUtf8().data() << endl;
   int ret = system(cmd.toUtf8().data());
 
   return ret;
@@ -92,95 +93,84 @@ void Device::close()
 void Device::getParams()
 {
   int i;
-   struct v4l2_queryctrl ctrl;
-   struct v4l2_control c;
-   const char *param;
-   param = "ueee";
+  struct v4l2_queryctrl ctrl;
+  struct v4l2_control c;
 
 #ifdef V4L2_CTRL_FLAG_NEXT_CTRL
-   /* Try the extended control API first */
-   ctrl.id = V4L2_CTRL_FLAG_NEXT_CTRL;
-   if(0 == v4l2_ioctl (fd, VIDIOC_QUERYCTRL, &ctrl)) {
-       do {
-           c.id = ctrl.id;
-           ctrl.id |= V4L2_CTRL_FLAG_NEXT_CTRL;
-           if(ctrl.flags & V4L2_CTRL_FLAG_DISABLED) {
-               continue;
-           }
-           if(ctrl.type != V4L2_CTRL_TYPE_INTEGER &&
-              ctrl.type != V4L2_CTRL_TYPE_BOOLEAN &&
-              ctrl.type != V4L2_CTRL_TYPE_MENU) {
-               continue;
-           }
-           if(v4l2_ioctl(fd, VIDIOC_G_CTRL, &c) == 0) {
-               //printf("1. %u:%31s:%d\n", c.id, ctrl.name, c.value);
-               //param = (char*)ctrl.name;
-              // cout << "[" << param << "] [" << c.id << "] [" << c.value << "]" << endl;
-               params.insert(QString( (char*)ctrl.name ), c.id);
-
-           }
-       } while(0 == v4l2_ioctl (fd, VIDIOC_QUERYCTRL, &ctrl));
-   } else
+  /* Intentar primero la API de control extendida */
+  ctrl.id = V4L2_CTRL_FLAG_NEXT_CTRL;
+  if(0 == v4l2_ioctl (fd, VIDIOC_QUERYCTRL, &ctrl)) {
+    do {
+      c.id = ctrl.id;
+      ctrl.id |= V4L2_CTRL_FLAG_NEXT_CTRL;
+      if(ctrl.flags & V4L2_CTRL_FLAG_DISABLED) {
+        continue;
+      }
+      if(ctrl.type != V4L2_CTRL_TYPE_INTEGER &&
+          ctrl.type != V4L2_CTRL_TYPE_BOOLEAN &&
+          ctrl.type != V4L2_CTRL_TYPE_MENU) {
+        continue;
+      }
+      if(v4l2_ioctl(fd, VIDIOC_G_CTRL, &c) == 0) {
+        params.insert(QString( (char*)ctrl.name ), c.id);
+      }
+    } while(0 == v4l2_ioctl (fd, VIDIOC_QUERYCTRL, &ctrl));
+  } else
 #endif
-   {
-       /* Check all the standard controls */
-       for(i=V4L2_CID_BASE; i<V4L2_CID_LASTP1; i++) {
-           ctrl.id = i;
-           if(v4l2_ioctl(fd, VIDIOC_QUERYCTRL, &ctrl) == 0) {
-               if(ctrl.flags & V4L2_CTRL_FLAG_DISABLED) {
-                   continue;
-               }
-               if(ctrl.type != V4L2_CTRL_TYPE_INTEGER &&
-                  ctrl.type != V4L2_CTRL_TYPE_BOOLEAN &&
-                  ctrl.type != V4L2_CTRL_TYPE_MENU) {
-                   continue;
-               }
-               c.id = i;
-               if(v4l2_ioctl(fd, VIDIOC_G_CTRL, &c) == 0) {
-                   //printf("2. %u:%31s:%d\n", i, ctrl.name, c.value);
-                   params.insert(QString( (char*)ctrl.name ), i);
-               }
-           }
-       }
+  {
+    /* Comprobar todos los controles estándard */
+    for(i=V4L2_CID_BASE; i<V4L2_CID_LASTP1; i++) {
+      ctrl.id = i;
+      if(v4l2_ioctl(fd, VIDIOC_QUERYCTRL, &ctrl) == 0) {
+        if(ctrl.flags & V4L2_CTRL_FLAG_DISABLED) {
+          continue;
+        }
+        if(ctrl.type != V4L2_CTRL_TYPE_INTEGER &&
+            ctrl.type != V4L2_CTRL_TYPE_BOOLEAN &&
+            ctrl.type != V4L2_CTRL_TYPE_MENU) {
+          continue;
+        }
+        c.id = i;
+        if(v4l2_ioctl(fd, VIDIOC_G_CTRL, &c) == 0) {
+          params.insert(QString( (char*)ctrl.name ), i);
+        }
+      }
+    }
 
-       /* Check any custom controls */
-       for(i=V4L2_CID_PRIVATE_BASE; ; i++) {
-           ctrl.id = i;
-           if(v4l2_ioctl(fd, VIDIOC_QUERYCTRL, &ctrl) == 0) {
-               if(ctrl.flags & V4L2_CTRL_FLAG_DISABLED) {
-                   continue;
-               }
-               if(ctrl.type != V4L2_CTRL_TYPE_INTEGER &&
-                  ctrl.type != V4L2_CTRL_TYPE_BOOLEAN &&
-                  ctrl.type != V4L2_CTRL_TYPE_MENU) {
-                   continue;
-               }
-               c.id = i;
-               if(v4l2_ioctl(fd, VIDIOC_G_CTRL, &c) == 0) {
-                   //printf("3. %u:%31s:%d\n", i, ctrl.name, c.value);
-
-                 params.insert(QString( (char*)ctrl.name ), i);
-               }
-           } else {
-               break;
-           }
-       }
-   }
+    /* Comprobar los posibles controles personalizados */
+    for(i=V4L2_CID_PRIVATE_BASE; ; i++) {
+      ctrl.id = i;
+      if(v4l2_ioctl(fd, VIDIOC_QUERYCTRL, &ctrl) == 0) {
+        if(ctrl.flags & V4L2_CTRL_FLAG_DISABLED) {
+          continue;
+        }
+        if(ctrl.type != V4L2_CTRL_TYPE_INTEGER &&
+            ctrl.type != V4L2_CTRL_TYPE_BOOLEAN &&
+            ctrl.type != V4L2_CTRL_TYPE_MENU) {
+          continue;
+        }
+        c.id = i;
+        if(v4l2_ioctl(fd, VIDIOC_G_CTRL, &c) == 0) {
+          params.insert(QString( (char*)ctrl.name ), i);
+        }
+      } else {
+        break;
+      }
+    }
+  }
 }
 
 void Device::listParams()
 {
-   QMapIterator<QString, int> it(params);
-   while (it.hasNext()) {
-       it.next();
-       cout << it.key().toUtf8().data() << ": " << it.value() << endl;
-   }
+  QMapIterator<QString, int> it(params);
+  while (it.hasNext()) {
+    it.next();
+    cout << it.key().toUtf8().data() << ": " << it.value() << endl;
+  }
 }
 
 int Device::setParam(const QString &name, int value)
 {
-//  __u32 id;
-//  __s32 value;
   struct v4l2_queryctrl ctrl;
   struct v4l2_control c;
 
@@ -189,19 +179,19 @@ int Device::setParam(const QString &name, int value)
 
   ctrl.id = params.value(name);
 
+  cout << " > setParam(" << name.toUtf8().data() << ", " << value << ")" << endl;
 
   if(v4l2_ioctl(fd, VIDIOC_QUERYCTRL, &ctrl) == 0) {
-
     c.id = params.value(name);
     c.value = value;
     if(v4l2_ioctl(fd, VIDIOC_S_CTRL, &c) != 0) {
-      fprintf(stderr, "Failed to set control \"%s\": %s\n", name.toUtf8().data(), strerror(errno));
+      fprintf(stderr, "Error al establecer el control \"%s\": %s\n", name.toUtf8().data(), strerror(errno));
     }
   }
   else {
-    fprintf(stderr, "Error querying control %s: %s\n", name.toUtf8().data(), strerror(errno));
+    fprintf(stderr, "Error al obtener el control %s: %s\n", name.toUtf8().data(), strerror(errno));
     return EXIT_FAILURE;
- }
+  }
 
   return EXIT_SUCCESS;
 }
--- main.cpp
+++ main.cpp
@@ -4,42 +4,78 @@
 
 int main(int argc, char *argv[])
 {
-    Device dev("/dev/video0");
+  // Dispositivo por defecto
+  QString d = "/dev/video0";
+  opterr = 0;
+  int c;
 
-    if( dev.open() != EXIT_SUCCESS )
-      return EXIT_FAILURE;
-
-    //dev.setROI(50, 50, 320, 240);
-    DataBase db;
-    QStringList plist = db.getParams();
-    Takes takes = db.getTakes();
-    QDateTime now = QDateTime::currentDateTime();
-    dev.setBaseDir(QString(IMG_DIR).append(now.toString("yyyy-MM-dd/hhmm/")));
-
-    // Para cada captura de la toma
-    for (int i = 0; i < takes.size(); i++) {
-     // cout << endl << "*** Toma " << takes.at(i).value("id").toUtf8().data() << endl;
-      QString log = now.toString("\"yyyy-MM-dd\"") + ", " + now.toString("\"hh:mm:ss\"");
-      log += ", " + takes.at(i).value("id");
-
-      // Establecer los parámetros de la cámara
-      Params take = takes.at(i);
-      for (int p = 0; p < plist.size(); ++p){
-        QString par = plist.at(p++);
-        int val = take.value(par).toInt();
-       // cout << "> setParam(" << par.toUtf8().data() << ", " << val << ")" << endl;
-        dev.setParam(par, val);
-        log += ", " + QString::number(val);
-      }
-
-      // Capturar y guardar imagen
-      QString path = QString("/").append(QString(takes.at(i).value("id")).rightJustified(2,'0')).append(".bmp");
-      dev.capture(path);
-
-      // Crear el log
-      db.log(log);
+  // Procesar los argumentos
+  while ((c = getopt (argc, argv, "d:")) != -1)
+    switch (c)
+    {
+      case 'd':
+        d = optarg;
+        break;
+      case '?':
+        if (optopt == 'd')
+          fprintf (stderr, "La opción -%c requiere un argumento.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Opción desconocida `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+              "Unknown option character `\\x%x'.\n",
+              optopt);
+        return EXIT_FAILURE;
+      default:
+        abort ();
     }
 
-    dev.close();
-    return EXIT_SUCCESS;
+  // Abrir dispositivo de vídeo
+  Device dev(d);
+  if(dev.open() != EXIT_SUCCESS)
+    return EXIT_FAILURE;
+
+  // Obtener configuración de la base de datos
+  DataBase db;
+  QStringList plist = db.getParams();
+  Takes takes = db.getTakes();
+
+  // Obtener fecha actual
+  QDateTime now = QDateTime::currentDateTime();
+  dev.setBaseDir(QString(IMG_DIR).append(now.toString("yyyy-MM-dd/hhmm/")));
+
+  // Establecer región de interés de la imagen
+  //dev.setROI(50, 50, 320, 240);
+
+  // Para cada captura de la toma
+  for (int i = 0; i < takes.size(); i++) {
+    QString log = now.toString("\"yyyy-MM-dd\"") + ", " + now.toString("\"hh:mm:ss\"");
+    log += ", " + takes.at(i).value("id");
+
+    // Establecer los parámetros de la cámara
+    Params take = takes.at(i);
+    for (int p = 0; p < plist.size(); ++p){
+      QString par = plist.at(p++);
+      int val = take.value(par).toInt();
+
+      // Exposicón en modo manual
+      if(par == "Exposure (Absolute)")
+        dev.setParam("Exposure, Auto", 1);
+
+      dev.setParam(par, val);
+      log += ", " + QString::number(val);
+    }
+
+    // Capturar y guardar imagen
+    QString path = QString("/").append(QString(takes.at(i).value("id")).rightJustified(2,'0')).append(".bmp");
+    dev.capture(path);
+
+    // Crear registro en el log
+    db.log(log);
+  }
+
+  // Cerrar dispositivo de vídeo
+  dev.close();
+
+  return EXIT_SUCCESS;
 }
